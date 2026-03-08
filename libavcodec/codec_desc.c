e9c27499444fda972e2d2bac01b9b1d2883130c1
aliofwind <35068322+aliofwind@users.noreply.github.com>
2026-03-08 15:37:31 +0800

    Update codec_desc.c

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -3553,6 +3553,7 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .long_name = NULL_IF_CONFIG_SMALL("LC3 (Low Complexity Communication Codec)"),
         .props     = AV_CODEC_PROP_INTRA_ONLY | AV_CODEC_PROP_LOSSY,
     },
+    {
         .id        = AV_CODEC_ID_DRA,
         .type      = AVMEDIA_TYPE_AUDIO,
         .name      = "dra",
